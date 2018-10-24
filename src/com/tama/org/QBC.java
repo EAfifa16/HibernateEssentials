fe5f17dcf70736f24e19d9382b67ada71c04d397
Mr.T <tamasishmullick@gmail.com>
2018-10-24 20:54:13 +0530

    QBC

--- src/com/tama/org/QBC.java
+++ src/com/tama/org/QBC.java
@@ -8,7 +8,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.criterion.SimpleExpression;
 
 public class QBC {
 
@@ -32,9 +31,9 @@ public class QBC {
 			System.out.println("\n customers by : Id");
 			Criteria ct2 = session.createCriteria(Customer.class);
 			ct2.add(Restrictions.eq("id", 2));
-			Customer cust=(Customer)ct2.uniqueResult();
+			Customer cust = (Customer) ct2.uniqueResult();
 			System.out.println(cust);
-			
+
 			// 3. getting Customer by city
 			System.out.println("\n Customer by : city");
 			Criteria ct3 = session.createCriteria(Customer.class);
@@ -42,17 +41,17 @@ public class QBC {
 			List<Customer> list1 = ct3.list();
 			for (Customer cust1 : list1) {
 				System.out.println(cust1);
-			}			
-			
+			}
+
 			// 4. getting Customer using city and Id (Using Criterion)
 			System.out.println("\n Customer by city and Id");
-			Criteria ct4=session.createCriteria(Customer.class);
-			Criterion city=Restrictions.eq("city","ranchi"); 
-			Criterion id=Restrictions.eq("id", 1); 
-			ct4.add(Restrictions.and(city,id));
-			Customer cust1=(Customer)ct4.uniqueResult();
+			Criteria ct4 = session.createCriteria(Customer.class);
+			Criterion city = Restrictions.eq("city", "ranchi");
+			Criterion id = Restrictions.eq("id", 1);
+			ct4.add(Restrictions.and(city, id));
+			Customer cust1 = (Customer) ct4.uniqueResult();
 			System.out.println(cust1);
-			
+
 			tx.commit();
 			session.close();
 
